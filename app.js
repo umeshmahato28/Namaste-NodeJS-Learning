d1b27ffc8b7f6152dd2b9997bdfb6529c2a13fe6
umeshmahato28 <mahatoumesh94@gmail.com>
2025-01-08 18:41:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const {x, calculateSum} = require('./sum.js');
+
+var name = "Node JS";
+
+var a = 20;
+var b = 30;
+
+console.log(name);
+console.log(a+b);
+
+calculateSum(a,b);
+console.log(x);
+
+
+// console.log(globalThis===global);
+
---
+++ sum.js
@@ -0,0 +1,9 @@
+console.log("Sum module executed");
+var x = "Hello";
+
+function calculateSum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+module.exports = {x,  calculateSum};
