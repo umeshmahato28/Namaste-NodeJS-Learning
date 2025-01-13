2be4b2a023fa091bc1f4d386aec592b17ac80563
umeshmahato28 <mahatoumesh94@gmail.com>
2025-01-13 18:02:42 +0530

    blocking code in node

---
+++ async.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const https = require("https");
+
+console.log("hello world");
+
+var a = 232112;
+var b = 2323;
+
+https.get("https://dummyjson.com/products/1", (res) => {
+      console.log("Fetched Data Successfully");
+});
+
+setTimeout(() => {
+      console.log("Timeout Completed");
+},3000);
+
+fs.readFileSync("./file.txt", "utf8");
+
+fs.readFile("./file.txt", "utf8", (err, data) => {
+      console.log("FIle data : ", data);
+});
+
+function multiplyfn(a, b) {
+      const result = a * b;
+      return result;
+} 
+
+var c = multiplyfn(a, b);
+
+console.log("Multiplication of a and b is : ", c);
---
+++ blocking.js
@@ -0,0 +1,22 @@
+const crypto = require("crypto");
+
+console.log("Strat");
+
+var a = 1078698;
+var b = 20986;
+
+crypto.pbkdf2Sync("password", "salt", 500000, 50, "sha512");
+console.log("First Key is Generated");
+
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, key) => {
+  console.log("Second Key is Generated");
+});
+
+function multiplyFn(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = multiplyFn(a, b);
+
+console.log("Multiplication result is : ", c);
